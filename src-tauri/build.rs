e0206a6f7787699d59453e5d2dc70ff4c66a3a37
SKunAether <2820241167@qq.com>
2026-08-13 09:09:39 +0800

    初始提交

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    // 硬编码Ubuntu22.04 pkgconfig目录，无视外部环境变量
+    std::env::set_var(
+        "PKG_CONFIG_PATH",
+        "/usr/lib/x86_64-linux-gnu:/usr/share/pkgconfig",
+    );
+    std::env::set_var("PKG_CONFIG_LIBDIR", "/usr/lib/x86_64-linux-gnu/pkgconfig");
     tauri_build::build()
 }
