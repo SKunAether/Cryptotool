573f68e82c1478de0a0fa9bc21c4f341e2b6a7e3
SKunAether <2820241167@qq.com>
2026-08-13 09:30:07 +0800

    初始提交

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    std::env::set_var(
-        "PKG_CONFIG_PATH",
-        "/usr/lib/x86_64-linux-gnu:/usr/share/pkgconfig",
-    );
-    std::env::set_var("PKG_CONFIG_LIBDIR", "/usr/lib/x86_64-linux-gnu/pkgconfig");
     tauri_build::build()
 }
